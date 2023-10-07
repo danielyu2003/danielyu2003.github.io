7870b004e3996eb65c1ea916d5058535ded3d993
Daniel Yu <88413927+danielyu2003@users.noreply.github.com>
2023-10-07 02:39:12 -0400

    shell script for adding posts

---
+++ add_post.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+HELPMSG="Usage: ./add_post"
+DATE=$(date '+%Y-%m-%d')
+
+if [ $# != 0 ]; then
+	echo $HELPMSG
+	exit 1
+fi
+
+mkdir content/archive/$DATE
+cd content/archive/$DATE
+touch index.md
+cat << EOF > index.md
++++
+title = "tmp"
+date = $DATE
+[extra]
+footer = "tmp"
++++
+
+insert summary
+
+<!-- more -->
+
+insert rest of content
+
+EOF
+
+echo created index.md at content/archive/$DATE
